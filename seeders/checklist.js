d1c78d9d065ba417d5ecbdf7ee6125a6e1ae2ffb
wonkatin <j.drawers@gmail.com>
2021-03-23 16:15:23 -0500

    started working on seeder file

--- seeders/checklist.js
+++ seeders/checklist.js
@@ -22,7 +22,112 @@ seeder.connect('mongodb://localhost/test_tripTracker', function (){
 
 const data = [
     {
-        'model': ''
+        'model': 'Checklist',
+        'documents' : [
+            {
+                'title': 'default list',
+                'date': '',
+                'clothing and accessories': [
+                    {
+                      'itemName': 'sleepwear',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'underwear',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'socks/stockings',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'undershirts/bras',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'pants',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'shorts',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'skirts',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'dresses',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 't-shirts',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'dress shirts',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'sweaters/sweatshirts',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'formal wear',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'swimsuit',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'coat/jacket/rainwear',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'hat',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'gloves',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'scarf',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'umbrella',
+                      'checked': false  
+                    },
+                    {
+                      'itemName': 'shoes',
+                      'checked': false  
+                    }
+                ],
+                'toiletries': [
+                    {
+                      'itemName': '',
+                      'checked': false  
+                    },
+                    
+                ],
+                'miscellaneous': [
+                    {
+                      'itemName': '',
+                      'checked': false  
+                    },
+                    
+                ],
+                'carry-on': [
+                    {
+                      'itemName': '',
+                      'checked': false  
+                    },
+                    
+                ]
+            }
+        ]
 
     }
 ]
